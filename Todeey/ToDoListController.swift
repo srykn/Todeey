7e0455ce1260d7eb2e94a8d48475f12914d59a9b
srykn <surya.kona@gmail.com>
2018-06-25 14:47:41 +0100

    [SK] Todeey app with data source and delegate methods

---
+++ Todeey/ToDoListController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Todeey
+//
+//  Created by Surya Kona on 25/06/2018.
+//  Copyright © 2018 hive. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListController: UITableViewController {
+    
+    let items = ["Buy stuff", "Sell stuff", "Do stuff"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK - TableView Data Source methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+    //MARK - TableView Delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
